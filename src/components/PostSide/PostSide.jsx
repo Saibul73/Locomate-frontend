c9fe7351fde550f9590c61e88a44ee94a407bd22
saibul <saibulfariskt3@gmail.com>
2023-02-04 18:17:52 +0530

    production

--- src/components/PostSide/PostSide.jsx
+++ src/components/PostSide/PostSide.jsx
@@ -2,12 +2,25 @@ import React from "react";
 import "./PostSide.css";
 import PostShare from "../PostShare/PostShare";
 import Posts from "../Posts/Posts";
+import { useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
 
-export default function PostSide({location}) {
+export default function PostSide({ location }) {
+  const { user } = useSelector((state) => state.authReducer.authData);
+  const params = useParams();
+  const id = params.id;
   return (
     <div className="PostSide">
-      <div className={location === "profilePage"?'':"share_component"}><PostShare /></div>
-      <div className={location === "profilePage"?'':"post_component"}><Posts /></div>
+      {location !=="profilePage" || user._id === id ? (
+        <div className={location === "profilePage" ? "" : "share_component"}>
+          <PostShare />
+        </div>
+      ) : (
+        ""
+      )}
+      <div className={location === "profilePage" ? "" : "post_component"}>
+        <Posts />
+      </div>
     </div>
   );
 }
